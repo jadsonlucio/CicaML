39fdd99750ecf7e43d24913fee7a0982d4989694
jadsonlucio <jadsonaluno@hotmail.com>
2021-12-29 11:57:02 -0300

    setup: update version to 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cicaml",
-    version="0.1.2",
+    version="0.2.0",
     description="CICAML",
     packages=find_packages(),
     install_requires=[
