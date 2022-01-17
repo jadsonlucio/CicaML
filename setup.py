a427a01fc96e095df9c82c86303efc70a76c793b
jadsonlucio <jadsonaluno@hotmail.com>
2022-01-17 01:51:32 -0300

    build: bump version from 0.2.0 to 0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cicaml",
-    version="0.2.0",
+    version="0.2.1",
     description="CICAML",
     packages=find_packages(),
     install_requires=[
